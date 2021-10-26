5643e7a2fd1bc9be8e441727e25ee0500e120369
Olivermainz <Olivernyrnbergmainz@gmail.com>
2021-10-26 09:20:52 +0200

    Menu item Create product

---
+++ ConsoleApp/ConsoleApp/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uge34Library.Models
+{
+    class Menu
+    {
+        private const int MENU_ARRAY_SIZE = 5;
+        private string Title;
+        private int ItemCount = 0;
+        private MenuItem[] MenuItems = new MenuItem[MENU_ARRAY_SIZE];
+
+
+        public Menu(string menuTitle)
+        {
+            this.Title = menuTitle;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(Title + "\n");
+            for (int i = 0; i < ItemCount; i++)
+            {
+                Console.WriteLine("  " + MenuItems[i].Title);
+            }
+            //Console.WriteLine("\nTryk menupunkt eller 0 for at afslutte");
+        }
+
+        public void AddMenuItem(string title)
+        {
+            MenuItem menuLine = new MenuItem();
+            menuLine.Title = title;
+            MenuItems[ItemCount] = menuLine;
+            ItemCount++;
+        }
+    }
+}
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.IO;
+using Uge34Library;
 
-namespace ConsoleApp
+namespace Uge34Library.Models
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu mainMenu = new Menu("Webshop Menu");
+            DirectoryInfo[] Prod = new DirectoryInfo(@"c:\").GetDirectories();
+
+
+            mainMenu.AddMenuItem("1. Vis Produkt");
+            mainMenu.AddMenuItem("2. Opret Produkt");
+            mainMenu.AddMenuItem("3. Slet Produkt");
+            mainMenu.AddMenuItem("4. Rediger Produkt");
+            mainMenu.AddMenuItem("5. Afslut\n");
+
+            startOver:
+            mainMenu.Show();
+            string userInput = Console.ReadLine();
+            switch (userInput) 
+            {
+                case "1":
+                    {
+                        break;
+                    }
+                case "2":
+                    {
+
+                        createProduct product= new createProduct();
+                        product.CreateProduct();
+                        break;
+                    }
+
+
+
+
+
+            }
+
         }
     }
 }
