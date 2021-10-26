f577185307692942757a58ba60f7c4995f75bd98
Alexander Olsen <Alexandervictorolsen@hotmail.com>
2021-10-26 09:56:00 +0200

    ProductRepo added and applicationDbContext to lib

---
+++ Uge34Library/Uge34Library/Repo/ProductRepo.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uge34Library.Data;
+using Uge34Library.Models;
+
+namespace Uge34Library.Repo
+{
+    public class ProductRepo : IRepository<Product>
+    {
+
+        private ApplicationDbContext _context;
+        private DbSet<Product> table;
+
+        public ProductRepo(ApplicationDbContext _context)
+        {
+            this._context = _context;
+            table = _context.Set<Product>();
+        }
+        public void CreateProduct(Product obj)
+        {
+            table.Add(obj);
+        }
+
+        public void DeleteProduct(Product obj)
+        {
+            table.Remove(obj);
+        }
+
+        public List<Product> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public Product GetByid(int id)
+        {
+            return table.Find(id);
+        }
+
+        public void Update(Product obj)
+        {
+            table.Attach(obj);
+            table.Update(obj);
+        }
+    }
+}
